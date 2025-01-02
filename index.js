d6a96095eb1455a0fd22242818096a721f38d7f6
AndySmithQA <andy.smith@qa.com>
2025-01-02 14:01:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('open-menu-btn').addEventListener('click', function() {
+    var menuModal = new bootstrap.Modal(document.getElementById('menuModal'));
+    menuModal.show();
+});
