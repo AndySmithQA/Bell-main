22093cfe33da9348509756667c270f16dbce1073
AndySmithQA <andy.smith@qa.com>
2025-01-10 14:52:03 +0000

    Final Draft

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('open-menu-btn').addEventListener('click', function() {
-    var menuModal = new bootstrap.Modal(document.getElementById('menuModal'));
-    menuModal.show();
-});
